80b15b1091bbf432bbc9997579595f0927b9f7a8
Aleksandr <Serov_ae@list.ru>
2020-04-09 20:01:49 +0300

    Создал 2 контроллера
    Создал 2 контроллера:
    1. Меню
    2. Игра

---
+++ Who Wants to Be a Millionaire?/Controllers/GameViewController.swift
@@ -0,0 +1,22 @@
+//
+//  GameViewController.swift
+//  Who Wants to Be a Millionaire?
+//
+//  Created by Aleksandr Serov on 09.04.2020.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+  
+
+}
