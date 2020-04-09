fbadcc7b02cd8ed96da801ea129cd22b68763892
Aleksandr <Serov_ae@list.ru>
2020-04-09 21:35:59 +0300

    Создал класс с Вопросами и написал логику игры

--- Who Wants to Be a Millionaire?/Controllers/GameViewController.swift
+++ Who Wants to Be a Millionaire?/Controllers/GameViewController.swift
@@ -9,14 +9,68 @@
 import UIKit
 
 class GameViewController: UIViewController {
-
+    
+    var qNumber = Int()
+    var answerNumber = Int()
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet var button: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        print(button)
+        
+        createQuestions()
     }
     
-
-  
-
+    func createQuestions() {
+        
+        if  gameQuestions.count > 0 {
+            qNumber = 0
+            questionLabel.text = gameQuestions[qNumber].questions
+            answerNumber = gameQuestions[qNumber].numbberOfAnswer
+            
+            for i in 0..<button.count {
+                button[i].setTitle(gameQuestions[qNumber].answers[i], for: UIControl.State.normal)
+            }
+            
+            gameQuestions.remove(at: qNumber)
+            
+        } else {
+            NSLog("Done!")
+        }
+    }
+    
+    @IBAction func button1(_ sender: Any) {
+        if answerNumber == 0 {
+            createQuestions()
+        } else {
+            NSLog("Wrong")
+        }
+    }
+    
+    @IBAction func button2(_ sender: Any) {
+        if answerNumber == 1 {
+                   createQuestions()
+               } else {
+                   NSLog("Wrong")
+               }
+    }
+    
+    @IBAction func button3(_ sender: Any) {
+        if answerNumber == 2 {
+                   createQuestions()
+               } else {
+                   NSLog("Wrong")
+               }
+    }
+    
+    @IBAction func button4(_ sender: Any) {
+        if answerNumber == 3 {
+                   createQuestions()
+               } else {
+                   NSLog("Wrong")
+               }
+    }
+    
 }
