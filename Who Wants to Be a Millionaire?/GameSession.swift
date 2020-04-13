6e54480f97e85ee560a890a92f9afa828b68f19e
Aleksandr <Serov_ae@list.ru>
2020-04-13 20:04:13 +0300

    Реализация паттерна Singltone и Delegate

---
+++ Who Wants to Be a Millionaire?/GameSession.swift
@@ -0,0 +1,31 @@
+//
+//  GameSession.swift
+//  Who Wants to Be a Millionaire?
+//
+//  Created by Aleksandr Serov on 10.04.2020.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import Foundation
+
+class GameSession {
+    
+    var answersCount = gameQuestions.count
+    var rightAnswerCount = 0
+    var gamePoint = Int()
+    var pointForRightAnswer = 1000
+    
+}
+
+class Game {
+    
+    var gameSession = GameSession()
+    static let Shared = Game()
+    private init() { }
+    
+}
+
+extension GameSession: GameDelegate {
+    func finishGame(rightAnswer: Int, Points: Int) {
+    }
+}
